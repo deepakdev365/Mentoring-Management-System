078eed01fc68a83a8060592a8ede66b663bb7da1
Ramesh Pradhan <rameshpradhan4651@gmail.com>
2026-01-30 11:10:29 +0530

    Refactor mentor login component for routing

--- Front-end/mentor_management_system/src/app/components/mentor-login.component/mentor-login.component.ts
+++ Front-end/mentor_management_system/src/app/components/mentor-login.component/mentor-login.component.ts
@@ -1,36 +1,34 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { MentorService } from '../../services/mentor.service';
 
 @Component({
   selector: 'app-mentor-login',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  templateUrl: './mentor-login.component.html'
+  templateUrl: './mentor-login.component.html',
 })
 export class MentorLoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
 
-  mentor = {
-    email: '',
-    password: ''
-  };
-
-  successMsg = '';
-  errorMsg = '';
-
-  constructor(private mentorService: MentorService) {}
+  constructor(
+    private mentorService: MentorService,
+    private router: Router
+  ) {}
 
   login() {
-    this.mentorService.login(this.mentor).subscribe({
-      next: (res: string) => {
-        this.successMsg = res;
-        this.errorMsg = '';
-      },
-      error: () => {
-        this.successMsg = '';
-        this.errorMsg = 'Invalid email or password';
-      }
-    });
-  }
+  this.mentorService.login(this.email, this.password).subscribe({
+    next: () => {
+      this.router.navigate(['/mentor-dashboard']);
+    },
+    error: () => {
+      this.errorMessage = 'Invalid email or password';
+    }
+  });
+}
+
 }
