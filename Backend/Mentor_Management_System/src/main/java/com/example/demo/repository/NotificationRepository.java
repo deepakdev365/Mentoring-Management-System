8b96476630cb1bc04de8e7af98d78b49378d9770
Rakeshjala <rakeshjala671@gmail.com>
2026-02-09 16:10:43 +0530

    Add files via upload

---
+++ Backend/Mentor_Management_System/src/main/java/com/example/demo/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Notification;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    List<Notification> findByReceiverIdAndReceiverRole(Long receiverId, String receiverRole);
+}
