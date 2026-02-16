848c26e5bc825db9b65780515a0176dc4d070bd4
Deepak Kumar Sahu <deepak3650901@gmail.com>
2026-02-16 15:10:35 +0530

    Added the multi sheet reading part.
    In StudentController added the multi sheet data reading part using the getNumberOfSheet() function of the workbook class of the poi library of Apache.

--- Backend/Mentor_Management_System/src/main/java/com/example/demo/controller/StudentController.java
+++ Backend/Mentor_Management_System/src/main/java/com/example/demo/controller/StudentController.java
@@ -29,73 +29,81 @@ public class StudentController {
 
         try {
             Workbook workbook = new XSSFWorkbook(file.getInputStream());
-            Sheet sheet = workbook.getSheetAt(0);
-
+            int numberOfSheets = workbook.getNumberOfSheets();
+            
             List<Student> students = new ArrayList<>();
+            for(int j = 0; j < numberOfSheets; j++) {
+            	Sheet sheet = workbook.getSheetAt(j);
 
-            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-
-                Row row = sheet.getRow(i);
-                if (row == null) continue;
-
-                Student student = new Student();
-
-                student.setFullName(getCellValue(row, 0));
-                
-                student.setFatherGuardianName(getCellValue(row, 1));
-                
-                student.setEmail(getCellValue(row, 2));
-                student.setDob(getCellValue(row, 3));
-                
-                student.setGender(getCellValue(row, 4));
-                student.setNationality(getCellValue(row, 5));
-                
-                student.setReligion(getCellValue(row, 6));
-                student.setEmergencyContact(getCellValue(row, 7));
-                student.setPhoneNumber(getCellValue(row, 8));
-
-                student.setLocalAddress(getCellValue(row, 9));
-                student.setPermanentAddress(getCellValue(row, 10));
-                
-                student.setCity(getCellValue(row, 11));
-                student.setState(getCellValue(row, 12));
-                student.setZipCode(getCellValue(row, 13));
-
-                
-                student.setAdmissionNumber(getCellValue(row, 14));
-                student.setApplicationNumber(getCellValue(row, 15));
-                student.setFeeCategory(getCellValue(row, 16));
-                
-                student.setDateOfAdmission(getCellValue(row, 17));
-                student.setProgram(getCellValue(row, 18));
-                student.setBranch(getCellValue(row, 19));
-                student.setSemester(getCellValue(row, 20));
-                student.setRollNo(getCellValue(row, 21));
-                
-                student.setEligibilityNumber(getCellValue(row, 22));
-                student.setPrnNo(getCellValue(row, 23));
-                student.setBatch(getCellValue(row, 24));
-                
-                student.setDepartment(getCellValue(row, 25));
-                
-                student.setPassword(getCellValue(row, 26));
-
-                
                
-                if (student.getEmail() == null || student.getPhoneNumber() == null) {
-                    continue; 
-                
+
+                for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+
+                    Row row = sheet.getRow(i);
+                    if (row == null) continue;
+
+                    Student student = new Student();
+                    student.setFullName(getCellValue(row, 0));
+                    
+                    student.setFatherGuardianName(getCellValue(row, 1));
+                    
+                    student.setEmail(getCellValue(row, 2));
+                    student.setDob(getCellValue(row, 3));
+                    
+                    student.setGender(getCellValue(row, 4));
+                    student.setNationality(getCellValue(row, 5));
+                    
+                    student.setReligion(getCellValue(row, 6));
+                    student.setEmergencyContact(getCellValue(row, 7));
+                    student.setPhoneNumber(getCellValue(row, 8));
+
+                    student.setLocalAddress(getCellValue(row, 9));
+                    student.setPermanentAddress(getCellValue(row, 10));
+                    
+                    student.setCity(getCellValue(row, 11));
+                    student.setState(getCellValue(row, 12));
+                    student.setZipCode(getCellValue(row, 13));
+
+                    
+                    student.setAdmissionNumber(getCellValue(row, 14));
+                    student.setApplicationNumber(getCellValue(row, 15));
+                    student.setFeeCategory(getCellValue(row, 16));
+                    
+                    student.setDateOfAdmission(getCellValue(row, 17));
+                    student.setProgram(getCellValue(row, 18));
+                    student.setBranch(getCellValue(row, 19));
+                    student.setSemester(getCellValue(row, 20));
+                    student.setRollNo(getCellValue(row, 21));
+                    
+                    student.setEligibilityNumber(getCellValue(row, 22));
+                    student.setPrnNo(getCellValue(row, 23));
+                    student.setBatch(getCellValue(row, 24));
+                    
+                    student.setDepartment(getCellValue(row, 25));
+                    
+                    student.setPassword(getCellValue(row, 26));
+
+                    
+                   
+                    if (student.getEmail() == null || student.getPhoneNumber() == null) {
+                        continue; 
+                    
+                    }
+
+                    students.add(student);
                 }
 
-                students.add(student);
+                studentService.saveAllStudent(students);
+                workbook.close();
+
+               
+
             }
-
-            studentService.saveAllStudent(students);
-            workbook.close();
-
             return "Students uploaded successfully : " + students.size();
-
-        } catch (Exception e) {
+            
+            	
+            }
+             catch (Exception e) {
             e.printStackTrace();
             return "Error while uploading students";
         }
