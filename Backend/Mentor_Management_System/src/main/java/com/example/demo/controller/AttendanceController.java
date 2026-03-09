3ffb2ffc586337ba8db26052193c176fe4dff742
Sheikh Ilyas Quadri <sheikhilyasquadri99@gmail.com>
2026-03-09 21:59:00 +0530

    Add files via upload

---
+++ Backend/Mentor_Management_System/src/main/java/com/example/demo/controller/AttendanceController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.service.AttendanceService;
+
+@RestController
+@RequestMapping("/attendance")
+@CrossOrigin("*")
+public class AttendanceController {
+
+    @Autowired
+    private AttendanceService attendanceService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadAttendance(
+            @RequestParam("attendance") MultipartFile file) {
+
+        try {
+
+            List<String> errors = attendanceService.uploadAttendance(file);
+
+            if (errors.isEmpty()) {
+                return ResponseEntity.ok("Attendance uploaded successfully");
+            } 
+            else {
+                return ResponseEntity.badRequest().body(errors);
+            }
+
+        } 
+        catch (Exception e) {
+            return ResponseEntity.internalServerError()
+                    .body("Error uploading attendance: " + e.getMessage());
+        }
+    }
+}
