18846b97fa7ac5f2fb7c8e294a50e84c773f69c9
Ramesh Pradhan <rameshpradhan4651@gmail.com>
2026-03-21 21:43:00 +0530

    upload mentor-login.component.ts

---
+++ Front-end/mentor_management_system/src/app/components/mentor-login.component/mentor-login.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MentorService } from '../../services/mentor.service';
+
+@Component({
+  selector: 'app-mentor-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './mentor-login.component.html',
+})
+export class MentorLoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(
+    private mentorService: MentorService,
+    private router: Router
+  ) {}
+
+login() {
+
+  if (!this.email || !this.password) {
+    this.errorMessage = "Please enter email and password";
+    return;
+  }
+
+  this.mentorService.login(
+    this.email.trim(),
+    this.password.trim()
+  ).subscribe({
+    next: (res: any) => {
+      console.log(res);
+
+      // ✅ STORE EMAIL (IMPORTANT)
+      localStorage.setItem('mentorEmail', this.email.trim());
+      localStorage.setItem('mentorName', this.email.trim());
+
+      this.router.navigate(['/mentor-dashboard']);
+    },
+    error: (err: any) => {
+      this.errorMessage = "Invalid email or password";
+    }
+  });
+}
+}
