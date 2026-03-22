0ce5d6e301d35c1f0ba4308950258cb077cd7ebb
Ramesh Pradhan <rameshpradhan4651@gmail.com>
2026-03-22 20:05:23 +0530

    student management system with repository layer

---
+++ Backend/Mentor_Management_System/src/main/java/com/example/demo/repository/StudentRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+	
+	 Optional<Student> findByRegistrationNumber(String registrationNumber);
+	 
+	 Optional<Student> findByEmailAndPassword(String email, String password);
+	 List<Student> findByRegistrationNumberIn(List<String> registrationNumbers);
+	 
+	 List<Student> findByMentor_Id(Integer mentorId);	 
+}
