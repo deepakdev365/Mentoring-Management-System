049016c841cc0ff7f94a956ef7665605fc44279a
Ramesh Pradhan <rameshpradhan4651@gmail.com>
2026-03-22 20:08:02 +0530

    Added StudentServiceImpl

---
+++ Backend/Mentor_Management_System/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Student;
+
+public interface StudentService {
+	Student saveStudent(Student student);
+	
+	void saveAllStudent(List<Student> students);
+
+	List<Student> getAllStudents();
+	
+    Student login(String email, String password);
+
+    void assignMentor(Long mentorId, List<String> registrationNumbers);
+
+
+	List<Student> getStudentsByMentorId(Integer id);
+
+	List<Student> getStudentsByMentorEmail(String email);
+}
---
+++ Backend/Mentor_Management_System/src/main/java/com/example/demo/service/StudentServiceImpl.java
@@ -0,0 +1,80 @@
+package com.example.demo.service;
+import com.example.demo.service.StudentMarksService;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.repository.MentorRepository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Mentor;
+import com.example.demo.model.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+    private final MentorRepository mentorRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository,
+                              MentorRepository mentorRepository) {
+        this.studentRepository = studentRepository;
+        this.mentorRepository = mentorRepository;
+    }
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public void saveAllStudent(List<Student> students) {
+        studentRepository.saveAll(students);
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student login(String email, String password) {
+        return studentRepository
+                .findByEmailAndPassword(email, password)
+                .orElseThrow(() -> new RuntimeException("Invalid email or password"));
+    }
+
+	@Override
+	public void assignMentor(Long mentorId, List<String> registrationNumbers) {
+		// TODO Auto-generated method stub
+		Mentor mentor = mentorRepository.findById(mentorId.intValue())
+	            .orElseThrow(() -> new RuntimeException("Mentor not found"));
+
+	    List<Student> students =
+	            studentRepository.findByRegistrationNumberIn(registrationNumbers);
+
+	    if (students.isEmpty()) {
+	        throw new RuntimeException("No students found");
+	    }
+
+	    for (Student student : students) {
+	        student.setMentor(mentor);
+	    }
+
+	    studentRepository.saveAll(students);
+
+	}
+	
+	@Override
+	
+	public List<Student> getStudentsByMentorId(Integer mentorId) {
+	    return studentRepository.findByMentor_Id(mentorId);
+	}
+
+	@Override
+	public List<Student> getStudentsByMentorEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+  
+}
